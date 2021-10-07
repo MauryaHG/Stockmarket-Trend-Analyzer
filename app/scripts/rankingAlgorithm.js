e896dd3eb610ea198ad37c24a41372ecb3ffb353
William Richie <wric0003@student.monash.edu>
2021-10-07 10:52:09 +0700

    consultant page now shows prediction of budget for next 3 months

--- app/scripts/rankingAlgorithm.js
+++ app/scripts/rankingAlgorithm.js
@@ -15,7 +15,7 @@ function getTable() {
     let budget = document.getElementById("budget").value;
     if (budget >= 50 && budget <= 10000) {
         let date = new Date();
-        date.setDate(date.getDate() - 30);
+        date.setDate(date.getDate() - 365);
         let start_date_string = date.toISOString().split('T')[0];
         let end_date_string = new Date().toISOString().split('T')[0];
         let listHTML = "";
@@ -52,19 +52,30 @@ function getTable() {
 
             let budgetPercentages = [30, 18, 10, 9, 8, 7, 6, 5, 4, 3];
             
-            i = 0
-            j = 0
+            i = 0;
+            j = 0;
+            total = 0;
+            totalOneMonth = 0;
+            totalTwoMonth = 0;
+            totalThreeMonth = 0;
             while (j < 10) {
                 change = list[i].percentageChange;
-                if (change < 100){
+                if (change < 1000){
                     currency = list[i].currency;
-                    color = list[i].changeColor
+                    color = list[i].changeColor;
                     let date = start_date_string + " to " + end_date_string;
                     budgetAllocated = (budget * budgetPercentages[j] / 100).toFixed(2);
+                    multiplier = 1 + (0.05 * change)/100;
+                    budgetOneMonth = (budgetAllocated * multiplier).toFixed(2);
+                    budgetTwoMonth = (budgetOneMonth * multiplier).toFixed(2);
+                    budgetThreeMonth = (budgetTwoMonth * multiplier).toFixed(2);
                     listHTML += "<tr> <td class=\"full-width mdl-data-table__cell--non-numeric\">" + currency + "</td>"
                     + "<td class=\"full-width mdl-data-table__cell\"><font color=" + color + ">" + change + "</font></td>"
                     + "<td class=\"full-width mdl-data-table__cell--non-numeric\">" + date + "</td>"
-                    + "<td class=\"full-width mdl-data-table__cell\">" + "$" + budgetAllocated + "</td></tr>";
+                    + "<td class=\"full-width mdl-data-table__cell\">" + "$" + budgetAllocated + "</td>"
+                    + "<td class=\"full-width mdl-data-table__cell\">" + "$" + budgetOneMonth + "</td>"
+                    + "<td class=\"full-width mdl-data-table__cell\">" + "$" + budgetTwoMonth + "</td>"
+                    + "<td class=\"full-width mdl-data-table__cell\">" + "$" + budgetThreeMonth + "</td></tr>";
                     j++;
                 } 
                 i++;
