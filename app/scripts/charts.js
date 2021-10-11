ae3dcf851796ce7d664eb99db862a0000f026233
mgam0010 <mgam0010@student.monash.edu>
2021-10-11 16:56:50 +0530

    added dropdown menu

--- app/scripts/charts.js
+++ app/scripts/charts.js
@@ -1,13 +1,37 @@
 var stockChart = 0
+const old_date = new Date();
+old_date.setFullYear(old_date.getFullYear()-1)
+const date_str = old_date.toISOString().substring(0, 10);
 
 window.onload = function () {
   
     var select = document.getElementById("selectCurrency");
-    const old_date = new Date();
-    old_date.setFullYear(old_date.getFullYear()-1)
-    const date_str = old_date.toISOString().substring(0, 10);
-    var dps1 = [], dps2= [];
+    
 
+    fetch("https://api.frankfurter.app/currencies")
+    .then((data) => data.json())
+    .then((data) => {
+      display(data);
+    });
+  
+  function display(data) {
+    const entries = Object.entries(data);
+    for (var i = 0; i < entries.length; i++) {
+      var opt = entries[i];
+      var el = document.createElement("option");
+      el.textContent = opt[1] +" (" + opt[0]  +")";
+      el.value = opt[0];
+      select.appendChild(el);
+    }
+    console.log(select)
+  }
+  
+}
+
+document.getElementById('selectCurrency').addEventListener('change', function() {
+  console.log('You selected: ', this.value);
+  curr_val = this.value
+  var dps1 = [], dps2= [];
     stockChart = new CanvasJS.StockChart("chartContainer",{
         theme: "light2",
         animationEnabled: true,
@@ -15,7 +39,7 @@ window.onload = function () {
           text:"StockChart with Date-Time Axis"
         },
         subtitles: [{
-          text: "Australian dollar"
+          text: curr_val
         }],
         charts: [{
           axisX: {
@@ -46,27 +70,11 @@ window.onload = function () {
     $.getJSON('https://api.frankfurter.app/'+date_str+'..?from=USD', function(data) {
         var rates = data.rates
         
-        display(data)
         for (var date in rates) {
             date_rate = rates[date]
-            dps1.push({x: new Date(date), y: Number(date_rate.AUD)});
-            dps2.push({x: new Date(date), y: Number(date_rate.AUD)});
+            dps1.push({x: new Date(date), y: Number(date_rate[curr_val])});
+            dps2.push({x: new Date(date), y: Number(date_rate[curr_val])});
         }
         stockChart.render();
 	});
-
-  function display(data) {
-    console.log(select)
-    const entries = Object.entries(data["rates"]);
-    currencies = Object.keys(entries[0][1])
-    for (var i = 0; i < currencies.length; i++) {
-      var opt = currencies[i];
-      var el = document.createElement("option");
-      el.textContent = opt;
-      el.value = opt;
-      select.appendChild(el);
-    }
-  }
-}
-
-
+});
